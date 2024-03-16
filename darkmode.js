a10f2ed38d20e30fe6caccd4fe7a478bd43e42d3
Christoph Maya <mail-github@mayach.de>
2024-03-16 07:53:07 +0100

    darkmode for typingmind

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,11 @@
 if (document.querySelector('[data-element-id="side-bar-background"]')) {
     document.querySelector('[data-element-id="side-bar-background"]').style.backgroundColor = "black";
+    document.querySelectorAll('[data-element-id="side-bar-background"] *').forEach(el => {
+        el.style.color = "white";
+    });
 }
+
+document.querySelectorAll('[data-element-id="user-message"]').forEach(function(element) {
+    element.style.backgroundColor = "#757575"; 
+    element.classList.remove('bg-blue-500'); 
+});
